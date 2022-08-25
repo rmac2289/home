e8995735602cb75d64c1f9a612a6d09165d6d902
Ross MacDonald <rossmacdonald@Rosss-Air.lan>
2022-08-24 21:09:59 -0700

    styling

--- pages/Cities.js
+++ pages/Cities.js
@@ -1,109 +1,111 @@
 import Link from "next/link";
-import _JSXStyle from 'styled-jsx/style';
+import _JSXStyle from "styled-jsx/style";
+
+// pick a random city
 
 export const Cities = ({ dataArray }) => {
   return (
     <>
-    <div className="citiesContainer">
-      {dataArray.map((city, index) => {
-        return (
-          <div key={index} className="city">
-            <div>
-              <Link href={city.cityGuideUrl}>
-                <a target="_blank">
-                  <h2 className="cityName">
-                    {city.cityName}, {city.state}
-                  </h2>
-                </a>
-              </Link>
-            </div>
-            {/* <img
+      <div className="citiesContainer">
+        {dataArray.map((city, index) => {
+          return (
+            <div key={index} className="city">
+              <div>
+                <Link href={city.cityGuideUrl}>
+                  <a target="_blank">
+                    <h2 className="cityName">
+                      {city.cityName}, {city.state}
+                    </h2>
+                  </a>
+                </Link>
+              </div>
+              {/* <img
               width="300"
               style={styles.img}
               src={city.image}
               alt={city.cityName}
             /> */}
-            <div className="topContainer">
-              <div className="iframeContainer">
-                <iframe
-                  src={city.mapIframe}
-                  width="300"
-                  height="200"
-                  style={{ border: 0, borderRadius: 5 }}
-                  allowFullScreen=""
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
-              </div>
-              <div className="stats">
-                <div className="homePriceP">
-                  <strong className="homePrice">Population: </strong>
-                  {city.population}
+              <div className="topContainer">
+                <div className="iframeContainer">
+                  <iframe
+                    src={city.mapIframe}
+                    className="iframe"
+                    style={{ border: 0, borderRadius: 5 }}
+                    width="300"
+                    height="200"
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
                 </div>
-                <p className="homePriceP">
-                  <strong className="homePrice">Median Home Price: </strong>
-                  {city.medianHomePrice}
-                </p>
-                <p className="homePriceP">
-                  <strong className="homePrice">Closest Airport: </strong>
-                  {city.closestAirport}, {city.timeToAirport} drive
-                </p>
-                <p className="homePriceP">
-                  <strong className="homePrice">Schools: </strong>
-                  {city.schoolGrade}
-                </p>
+                <div className="stats">
+                  <div className="homePriceP">
+                    <strong className="homePrice">Population: </strong>
+                    {city.population}
+                  </div>
+                  <p className="homePriceP">
+                    <strong className="homePrice">Median Home Price: </strong>
+                    {city.medianHomePrice}
+                  </p>
+                  <p className="homePriceP">
+                    <strong className="homePrice">Closest Airport: </strong>
+                    {city.closestAirport}, {city.timeToAirport} drive
+                  </p>
+                  <p className="homePriceP">
+                    <strong className="homePrice">Schools: </strong>
+                    {city.schoolGrade}
+                  </p>
+                </div>
               </div>
+              <p className="intro">{city.intro}</p>
             </div>
-            <p className="intro">{city.intro}</p>
-          </div>
-        );
-      })}
-      
-    </div>
-    <style jsx>{
-      `intro {
-        
-      }
-
-      .citiesContainer {
-        min-width: 610px;
-        max-width: 75%;
-        display: grid;
-      }
-      @media screen and (min-width: 1500px){
+          );
+        })}
+      </div>
+      <style jsx>{`
         .citiesContainer {
-          grid-template-columns: 1fr 1fr;
+          min-width: 610px;
+          max-width: 75%;
+          display: grid;
         }
-      }
-      .topContainer {
-        display: flex;
-        align-items: flex-start;
-      }
-      img {
-        borderRadius: 5;
-      }
-      .stats {
-        margin-bottom: 1rem;
-        margin-left: 1rem;
-      }
-      .city {
-        border: 1px solid white;
-        padding: 1rem;
-        border-radius: 5px;
-        margin: 10px;
-      }
-      .cityName {
-        color: orange;
-        border-bottom: 1px solid orange;
-        cursor: pointer;
-      }
-      .homePrice {
-        color: orange;
-      }
-      `
-}</style>
-</>
+        @media screen and (min-width: 1300px) {
+          .citiesContainer {
+            grid-template-columns: 1fr 1fr;
+          }
+        }
+        .topContainer {
+          display: flex;
+          align-items: flex-start;
+        }
+        img {
+          borderradius: 5;
+        }
+        .stats {
+          margin-bottom: 1rem;
+          margin-left: 1rem;
+        }
+        .city {
+          border: 1px solid white;
+          padding: 1rem;
+          border-radius: 5px;
+          margin: 15px;
+          box-shadow: 0.25rem 0.25rem #d3d3d3;
+        }
+
+        .cityName {
+          color: orange;
+          border-bottom: 1px solid orange;
+          cursor: pointer;
+          transition: all 0.25s linear;
+        }
+        .cityName:hover {
+          color: #ffd700;
+          border-bottom: 1px solid #ffd700;
+        }
+        .homePrice {
+          color: orange;
+        }
+      `}</style>
+    </>
   );
 };
-
-
--- pages/Nav.js
+++ pages/Nav.js
@@ -1,52 +1,60 @@
 import Link from "next/link";
 import _JSXStyle from "styled-jsx/style";
-import sheets from '../public/sheets.png'
-import Image from 'next/image'
+import sheets from "../public/sheets.png";
+import Image from "next/image";
 export const Nav = () => {
   return (
     <>
-        <nav className="nav">
-            <div className="linkContainer">
-            <Image src={sheets} alt="google sheets" layout="fixed" width="20px" height="30px"/> 
-
-            <Link href="https://docs.google.com/spreadsheets/d/1CtsOadJXDh8gPqpf6wjf80H-vskG8PxRQnRoLu2mdCI/edit#gid=0">
-                <a target="_blank">
-                go to google sheet
-                </a>
-            </Link>
-            </div>
-        </nav>
-        <style jsx>{`
+      <nav className="nav">
+        <div className="linkContainer">
+          <Link href="https://docs.google.com/spreadsheets/d/1CtsOadJXDh8gPqpf6wjf80H-vskG8PxRQnRoLu2mdCI/edit#gid=0">
+            <a target="_blank">
+              <Image
+                src={sheets}
+                alt="google sheets"
+                layout="fixed"
+                width="20px"
+                height="30px"
+              />
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <style jsx>{`
         .linkContainer {
-            border-radius: 20px;
-            height: 50px;
-            width: 200px;
-            background-color: white;
-            display: flex;
-            justify-content: space-evenly;
-            align-items: center;
-            margin: 0;
+          border-radius: 10px;
+          cursor: pointer;
+          height: 50px;
+          width: 50px;
+          background-color: white;
+          display: flex;
+          justify-content: space-evenly;
+          align-items: center;
+          margin: 0;
+          transition: all 0.25s linear;
         }
-          .nav {
-            position: fixed;
-            top: 0;
-            right: 0;
-            left: 0;
-            height: 100px;
-            padding: 1rem
-          }
-          div {
-            margin: 1rem;
-          }
-          a {
-            color: black;
-            font-size: 18px;
-            display:flex;
-            justify-content: space-between;
-            align-items: center;
-          }
-        `}</style>
-      
+        .linkContainer:hover {
+          transform: scale(1.1);
+        }
+        .nav {
+          position: fixed;
+          top: 0;
+          right: 0;
+          left: 0;
+          height: 100px;
+          padding: 1rem;
+        }
+        div {
+          margin: 1rem;
+        }
+        a {
+          color: black;
+          font-size: 18px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+      `}</style>
     </>
   );
 };
