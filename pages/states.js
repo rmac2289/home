91c16a4a3ccb800f9a011c71dad1e6b5f0cce934
Ross MacDonald <rossmacdonald@Rosss-Air.lan>
2022-08-27 14:20:20 -0700

    added park call

--- pages/states.js
+++ pages/states.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import Nav from "../components/Nav";
 import styles from "../styles/Home.module.css";
 
 export default function States({ taxes }) {
+  const [parks, setParks] = useState([]);
+  const [orParks, setOrParks] = useState([]);
+  const [waParks, setWaParks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let npsUrl = `https://developer.nps.gov/api/v1/parks?stateCode=wa%2Cor&api_key=3YdcawfOu9ProaUtfU3LiCKelraCFh1PgAdGSgTA`;
+      const npsRes = await fetch(npsUrl);
+      let npsBody = await npsRes.json();
+      setParks(npsBody);
+      setOrParks(npsBody.data.filter((park) => park.states === "OR"));
+      setWaParks(npsBody.data.filter((park) => park.states === "WA"));
+    })();
+  }, []);
+
   return (
     <>
       <Nav />
@@ -76,8 +92,8 @@ export async function getStaticProps() {
       : "https://gorgeous-meerkat-3dd227.netlify.app/api/taxes";
   const res = await fetch(url);
   const body = await res.json();
-  console.log(body);
   let taxes = body;
+
   // Props returned will be passed to the page component
   return { props: { taxes } };
 }
