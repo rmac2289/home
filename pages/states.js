2faba642215168d2234681d1ca25137051bc2990
Ross MacDonald <rossmacdonald@Rosss-Air.lan>
2022-08-27 07:30:47 -0700

    fixed api typo

--- pages/states.js
+++ pages/states.js
@@ -75,11 +75,8 @@ export async function getStaticProps() {
     process.env.ENVIRONMENT === "dev"
       ? "http://localhost:3000/api/taxes"
       : "https://gorgeous-meerkat-3dd227.netlify.app/api/taxes";
-  console.log(url);
   const res = await fetch(url);
-  console.log(res);
   const body = await res.json();
-  console.log(body);
   let taxes = body;
   // Props returned will be passed to the page component
   return { props: { taxes } };
